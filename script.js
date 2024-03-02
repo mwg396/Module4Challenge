f5164ff7bc6e2b703f2bedaacca19dbb179477f6
mwg396 <153116787+mwg396@users.noreply.github.com>
2024-03-02 13:03:52 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,215 @@
+let questions = [ 
+	{ 
+		prompt: `Which of the following is a string?`, 
+		options: [ 
+			"'Hello'", 
+			"Hello", 
+			"43110", 
+		], 
+		answer: "'Hello'", 
+	}, 
+
+	{ 
+		prompt: `Which of the following is a number Data type?`, 
+		options: [ 
+			"Hey", 
+			"'Hey'", 
+			"34", 
+			"'34'", 
+		], 
+		answer: "34", 
+	}, 
+
+	{ 
+		prompt: `True or False: Booleans are True/False Statements?`, 
+		options: [ 
+			"True",
+			"False",
+			"This question is confusing!"
+		], 
+		answer: "True", 
+	}, 
+
+]; 
+let questionsEl = 
+	document.querySelector( 
+		"#questions"
+	); 
+let timerEl = 
+	document.querySelector("#timer"); 
+let choicesEl = 
+	document.querySelector("#options"); 
+let submitBtn = document.querySelector( 
+	"#submit-score"
+); 
+let startBtn = 
+	document.querySelector("#start"); 
+let nameEl = 
+	document.querySelector("#name"); 
+let feedbackEl = document.querySelector( 
+	"#feedback"
+); 
+let reStartBtn = 
+	document.querySelector("#restart"); 
+
+let currentQuestionIndex = 0; 
+let time = questions.length * 5; 
+let timerId; 
+
+function quizStart() { 
+	timerId = setInterval( 
+		clockTick, 
+		1000 
+	); 
+	timerEl.textContent = time; 
+	let landingScreenEl = 
+		document.getElementById( 
+			"start-screen"
+		); 
+	landingScreenEl.setAttribute( 
+		"class", 
+		"hide"
+	); 
+	questionsEl.removeAttribute( 
+		"class"
+	); 
+	getQuestion(); 
+} 
+
+function getQuestion() { 
+	let currentQuestion = 
+		questions[currentQuestionIndex]; 
+	let promptEl = 
+		document.getElementById( 
+			"question-words"
+		); 
+	promptEl.textContent = 
+		currentQuestion.prompt; 
+	choicesEl.innerHTML = ""; 
+	currentQuestion.options.forEach( 
+		function (choice, i) { 
+			let choiceBtn = 
+				document.createElement( 
+					"button"
+				); 
+			choiceBtn.setAttribute( 
+				"value", 
+				choice 
+			); 
+			choiceBtn.textContent = 
+				i + 1 + ". " + choice; 
+			choiceBtn.onclick = 
+				questionClick; 
+			choicesEl.appendChild( 
+				choiceBtn 
+			); 
+		} 
+	); 
+} 
+
+function questionClick() { 
+	if ( 
+		this.value !== 
+		questions[currentQuestionIndex] 
+			.answer 
+	) { 
+		time -= 10; 
+		if (time < 0) { 
+			time = 0; 
+		} 
+		timerEl.textContent = time; 
+		feedbackEl.textContent = `Wrong! The correct answer was 
+		${questions[currentQuestionIndex].answer}.`; 
+		feedbackEl.style.color = "red"; 
+	} else { 
+		feedbackEl.textContent = 
+			"Correct!"; 
+		feedbackEl.style.color = 
+			"green"; 
+	} 
+	feedbackEl.setAttribute( 
+		"class", 
+		"feedback"
+	); 
+	setTimeout(function () { 
+		feedbackEl.setAttribute( 
+			"class", 
+			"feedback hide"
+		); 
+	}, 2000); 
+	currentQuestionIndex++; 
+	if ( 
+		currentQuestionIndex === 
+		questions.length 
+	) { 
+		quizEnd(); 
+	} else { 
+		getQuestion(); 
+	} 
+} 
+
+function quizEnd() { 
+	clearInterval(timerId); 
+	let endScreenEl = 
+		document.getElementById( 
+			"quiz-end"
+		); 
+	endScreenEl.removeAttribute( 
+		"class"
+	); 
+	let finalScoreEl = 
+		document.getElementById( 
+			"score-final"
+		); 
+	finalScoreEl.textContent = time; 
+	questionsEl.setAttribute( 
+		"class", 
+		"hide"
+	); 
+} 
+
+function clockTick() { 
+	time--; 
+	timerEl.textContent = time; 
+	if (time <= 0) { 
+		quizEnd(); 
+	} 
+} 
+
+function saveHighscore() { 
+	let name = nameEl.value.trim(); 
+	if (name !== "") { 
+		let highscores = 
+			JSON.parse( 
+				window.localStorage.getItem( 
+					"highscores"
+				) 
+			) || []; 
+		let newScore = { 
+			score: time, 
+			name: name, 
+		}; 
+		highscores.push(newScore); 
+		window.localStorage.setItem( 
+			"highscores", 
+			JSON.stringify(highscores) 
+		); 
+		alert( 
+			"Thanks"
+		); 
+	} 
+} 
+
+function checkForEnter(event) { 
+	if (event.key === "Enter") { 
+		saveHighscore(); 
+		alert( 
+			"Thanks"
+		); 
+	} 
+} 
+nameEl.onkeyup = checkForEnter; 
+
+submitBtn.onclick = saveHighscore; 
+
+startBtn.onclick = quizStart;
